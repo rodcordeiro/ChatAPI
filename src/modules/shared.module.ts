c0eb2d1825e8157b94ec68a582f07ee74474497e
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-01-31 14:16:40 -0300

    chore: Running prettier

--- src/modules/shared.module.ts
+++ src/modules/shared.module.ts
@@ -4,11 +4,7 @@ import { HealthModule } from '@/modules/health/health.module';
 import { UsersModule } from '@/modules/users/users.module';
 import { AuthModule } from '@/modules/auth/auth.module';
 @Module({
-  imports: [
-    HealthModule,
-    UsersModule,
-    AuthModule,
-  ],
+  imports: [HealthModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
